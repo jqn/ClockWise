ceeffee8c7155c4fd7d4e1336e30f36c26669c12
jQN <jqn@jQNs-MacBook-Pro.local>
2020-02-06 09:01:26 +0100

    feature: completed adding material button to react clockwise
    issue: #3

--- ReactClockWise/App/Components/StopWatch.js
+++ ReactClockWise/App/Components/StopWatch.js
@@ -1,145 +1,91 @@
-/**
- * App/Components/StopWatch.js
- */
-import React, { Component } from 'react';
-import time from '../Lib/TimeTracker';
-import moment from 'moment';
-import classnames from 'classnames';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Button from "@material-ui/core/Button";
 
-export default class Stopwatch extends Component {
-  constructor() {
-    super();
-    this.state = {};
-    // holds the actuall time
-    this.time = null;
-    // will be used to clear the interval
-    this.intervalId = null;
-    // used to switch from play to pause and viceversa
-    this.state.toggle = false;
-    // holds the formatted current time in HH:mm:ss
-    this.state.currentTime = null;
-  }
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     height: 200,
+//     width: 200,
+//     // backgroundColor: "#ff5f00",
+//     borderColor: "#ffc107",
+//     borderWidth: 10,
+//     borderRadius: "50%",
+//   },
+//   actionButton: {
+//     position: "relative",
+//     left: 0,
+//     right: 0,
+//     top: 0,
+//     bottom: 0,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     display: "flex",
+//     zIndex: 1000,
+//   },
+//   control: {
+//     padding: theme.spacing(2),
+//   },
+// }));
 
-  componentDidMount() {
-    if (this.props.todo.timestamp === '00:00:00') {
-      this.initializeTime();
-    } else {
-      this.rehydrateTime();
-    }
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  actionButton: {
+    borderColor: "#ffc107",
+    borderWidth: 10,
+    borderRadius: "50%",
+    width: 200,
+    height: 200,
+  },
+}));
 
-  componentWillUnmount() {
-    console.log('stopwatch unmounted');
-  }
+const StopWatch = () => {
+  const [spacing, setSpacing] = React.useState(2);
+  const classes = useStyles();
 
-  rehydrateTime = () => {
-    let time = this.props.todo.timestamp;
-    let timearray = time.split(':');
-    let hours = Number(timearray[0]) * 3600000;
-    let minutes = Number(timearray[1]) * 60000;
-    let seconds = Number(timearray[2]) * 1000;
-
-    this.time = hours + minutes + seconds;
-    this.setState({
-      currentTime: this.props.todo.timestamp
-    });
+  const handleChange = event => {
+    setSpacing(Number(event.target.value));
   };
 
-  initializeTime = () => {
-    this.time = 0;
-    this.setState({
-      currentTime: this.formatTime(this.getTime())
-    });
-  };
+  return (
+    // <Grid container className={classes.root} spacing={2}>
+    //   <Grid item xs={12}>
+    //     <Grid container justify="center" spacing={spacing}>
+    //       <Grid item xs={12}>
+    //         <Paper className={classes.paper} variant="outlined">
+    // <ButtonBase className={classes.actionButton}>
+    //   <Typography>Start</Typography>
+    // </ButtonBase>
+    //         </Paper>
+    //       </Grid>
+    //     </Grid>
+    //   </Grid>
+    // </Grid>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <Button variant="outlined" className={classes.actionButton}>
+              <Typography variant="h4">Start</Typography>
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-  runTimer = () => {
-    if (!this.state.toggle) {
-      this.startTime();
-    } else {
-      this.pauseTime();
-    }
-  };
-
-  startTime() {
-    this.createInterval();
-    this.toggle();
-    this.props.start('running', this.props.todo.id);
-  }
-
-  pauseTime = () => {
-    this.clearInterval();
-    this.toggle();
-    this.props.pause('paused', this.props.todo.id, this.state.currentTime);
-  };
-
-  resetTimer = () => {
-    this.initializeTime();
-    this.clearInterval();
-    this.setState({ toggle: false });
-    this.props.reset('reset', this.props.todo.id, '00:00:00', time());
-  };
-
-  toggle = () => {
-    this.setState({
-      toggle: !this.state.toggle
-    });
-  };
-
-  createInterval() {
-    this.intervalId = setInterval(this.updateTime, 100);
-  }
-
-  clearInterval() {
-    clearInterval(this.intervalId);
-  }
-
-  updateTime = () => {
-    this.time += 100;
-    const newTime = this.getTime();
-    const formattedTime = this.formatTime(newTime);
-    this.setState({ currentTime: formattedTime });
-  };
-
-  formatTime = momentJsObject => {
-    return momentJsObject.format('HH:mm:ss');
-  };
-
-  getTime = () => {
-    return moment.utc(this.time);
-  };
-
-  render() {
-    let toggleText = !this.state.toggle ? (
-      <i className="material-icons">play_arrow</i>
-    ) : (
-      <i className="material-icons">pause</i>
-    );
-    return (
-      <div
-        ref="time"
-        className="mdl-cell mdl-cell--12-col mdl-grid stopwatch"
-        id="time">
-        <div className="timer mdl-cell mdl-cell mdl-cell--4-col counter-wrapper">
-          <h5 className="counter">{this.state.currentTime}</h5>
-        </div>
-        <div className="controls mdl-cell  mdl-cell mdl-cell mdl-cell--4-col">
-          <button
-            className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab play"
-            onClick={this.runTimer}>
-            {toggleText}
-          </button>
-          <button
-            className="reset mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab replay"
-            onClick={this.resetTimer}>
-            <i className="material-icons">replay</i>
-          </button>
-          <button
-            className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect delete"
-            onClick={this.props.deleteTodo}>
-            <i className="material-icons">close</i>
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
+export default StopWatch;
--- ReactClockWise/App/Containers/App.js
+++ ReactClockWise/App/Containers/App.js
@@ -4,24 +4,12 @@
 "use strict";
 import React from "react";
 import Nav from "../Components/Nav";
-import Typography from "@material-ui/core/Typography";
+import Header from "../Components/Header";
+import StopWatch from "../Components/StopWatch";
 
 const App = () => (
   <Nav>
-    <Typography paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-      non enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-      imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-      Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-      Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-      adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-      viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin
-      fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-      tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-      varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-      Lorem donec massa sapien faucibus et molestie ac.
-    </Typography>
+    <StopWatch />
   </Nav>
 );
 
