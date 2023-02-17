67e48bd859afba96f8ae2c2a33503b3ec0414b70
jQN <joaquin_guardado@icloud.com>
2023-02-17 16:08:28 +0100

    Remove legacy apps

--- ReactClockWise/App/Actions/Index.js
+++
@@ -1,6 +0,0 @@
-/**
- * App/Actions/Index.js
- */
-import * as TodoActions from './Todos';
-
-export const ActionCreators = Object.assign({}, TodoActions);
--- ReactClockWise/Index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App/Containers/App";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
